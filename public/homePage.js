0ff3cb2311de32b8a50f7368a806da405c4de740
“krlo” <kirill.osadchenko@gmail.com>
2024-08-16 09:14:02 +0500

    Итоговая работа выполнена

--- public/homePage.js
+++ public/homePage.js
@@ -1,24 +1,104 @@
-'use strict';
+const logOutButton = new LogoutButton();
 
-const userForm = new UserForm();
-
-userForm.loginFormCallback = function (data) {
-    ApiConnector.login(data, respone => {
-            if(respone.success) {
-            location.reload();
-        } else {
-            userForm.setLoginErrorMessage(response.error);
-        }
-    });   
+logOutButton.action = function() {
+	ApiConnector.logout(response => {
+		if (response.success) {
+			location.reload();
+		}
+	})
 }
 
-userForm.registerFormCallback = function (data) {
-    ApiConnector.register(data, respone => {
-            if(respone.success) {
-            location.reload();
-        } else {
-        userForm.setRegisterErrorMessage(response.error);
-        }
-    }); 
+ApiConnector.current(response => {
+	if (response.success) {
+		ProfileWidget.showProfile(response.data)
+	}
+})
+
+const ratesBoard = new RatesBoard();
+
+let getStocks = () => {
+	ApiConnector.getStocks(response => {
+		if (response.success) {
+			ratesBoard.clearTable();
+			ratesBoard.fillTable(response.data)
+		}
+	})
+
 }
-    
+
+setInterval(getStocks, 60000);
+
+const moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback = function(data) {
+	ApiConnector.addMoney(data, response => {
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+			moneyManager.setMessage(response.success, 'Баланс пополнен');
+		} else {
+			moneyManager.setMessage(response.success, response.error);
+
+		}
+	})
+}
+
+
+moneyManager.conversionMoneyCallback = function(data) {
+	ApiConnector.convertMoney(data, response => {
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+			moneyManager.setMessage(response.success, 'Валюта конвертирована');
+		} else {
+			moneyManager.setMessage(response.success, response.error);
+
+		}
+	})
+}
+
+moneyManager.sendMoneyCallback = function(data) {
+	ApiConnector.transferMoney(data, response => {
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+			moneyManager.setMessage(response.success, 'Перевод выполнен');
+		} else {
+			moneyManager.setMessage(response.success, response.error);
+
+		}
+	})
+}
+
+const favoritesWidget = new FavoritesWidget();
+
+ApiConnector.getFavorites(response => {
+	if (response) {
+		favoritesWidget.clearTable();
+		favoritesWidget.fillTable(response.data);
+		moneyManager.updateUsersList(response.data);
+	}
+})
+
+favoritesWidget.addUserCallback = function(data) {
+	ApiConnector.addUserToFavorites(data, response => {
+		if (response) {
+			favoritesWidget.clearTable();
+			favoritesWidget.fillTable(response.data);
+			moneyManager.updateUsersList(response.data);
+			favoritesWidget.setMessage(response.success, 'Пользователь добавлен');
+		} else {
+			favoritesWidget.setMessage(response.success, response.error);
+		}
+	})
+}
+
+favoritesWidget.removeUserCallback = function(data) {
+	ApiConnector.removeUserFromFavorites(data, response => {
+		if (response) {
+			favoritesWidget.clearTable();
+			favoritesWidget.fillTable(response.data);
+			moneyManager.updateUsersList(response.data);
+			favoritesWidget.setMessage(response.success, 'Пользователь удален');
+		} else {
+			favoritesWidget.setMessage(response.success, response.error);
+		}
+	})
+}
